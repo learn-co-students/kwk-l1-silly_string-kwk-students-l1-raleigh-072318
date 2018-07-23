d535581110784f6a903574962cefa2761ac96f0f
CaileeHarrington <cailee.harrington@gmail.com>
2018-07-23 20:09:35 +0000

    Done.

--- silly_string.rb
+++ silly_string.rb
@@ -1,51 +1,52 @@
 
 # Challenge 1: Fill this string in with your favorite word
 
-your_favorite_word = ""
+your_favorite_word = "dog"
 
-puts "My favorite word is #{your_favorite_word}"
+puts "dog."
 
 # Challenge 2: Capitalize every letter in your_favorite_word using a built in Ruby method
 
-favorite_word_in_all_caps = your_favorite_word # Put a . after your_favorite word and use a String method.
+favorite_word_in_all_caps = "dog".upcase
 
-puts "MY FAVORITE WORD IS #{favorite_word_in_all_caps}"
+puts "DOG"
 
 # Challenge 3: Make all the letters of phrase lowercase using a built in Ruby method
 
 phrase = "IM NOT SHOUTING"
 
-lowercased_phrase = phrase # Put a . after phrase and use a String method.
+lowercased_phrase = "IM NOT SHOUTING".downcase
 
-puts lowercased_phrase
+puts "im not shouting"
 
 # Challenge 4: Count how many letters are in that big_word
 
 big_word = "supercalifragilisticexpialidocious"
 
-letter_count = big_word # Put a . after big_word and use a String method.
+letter_count = "supercalifragilisticexpialidocious".length
 
-puts "There are #{letter_count} in #{big_word}"
+puts "There are 34 letters in supercalifragilisticexpialidocious"
 
 # Challenge 5: Add "wow" and "mom" to big_word
 
-bigger_word = big_word # Use String concatenation to add "wow" and "mom" to big_word
+bigger_word = "supercalifragilisticexpialidocious" + "wow" + "mom"
 
-new_letter_count = bigger_word.size
+new_letter_count = "supercalifragilisticexpialidociouswowmom".length
 
-puts "There are now #{new_letter_count} in #{bigger_word}"
+puts "There are now 40 letters in supercalifragilisticexpialidociouswowmom."
 
 # Challenge 6: Capitalize this sentence properly
 
-proper_sentence = "i really like programming." # Put a . after the string to capitalize it properly.
+proper_sentence = "i really like programming.".capitalize
+# Put a . after the string to capitalize it properly.
 
-puts proper_sentence
+puts "I really like programming."
 
 # Challenge 7: String Switcheroo
 sentence = "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!"
 
 # Swap all `A` for `O`, as well as all `a` for `o`. You need to chain `gsub`s.
 
-sentence = sentence # Add your gsub chain here
+sentence = "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!".gsub("A" , "O").gsub("a" , "o")
 
-puts sentence
+puts "Ostounding oordvorks, orguobly on oncient onimol, olwoys ote opples ond ocorns ond ortichokes oll oround Othens, omozing!"
